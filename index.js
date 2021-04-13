cd2b5b270f8f277a865c956209ec6b7a339d688a
klarahapkova <80477797+klarahapkova@users.noreply.github.com>
2021-04-13 10:23:28 +0200

    Smazání zakomentovaných poznámek

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ const jePolePrazdne = (pole) => {
 };
 
 const kdoJeNaTahu = () => {
-  return document.querySelector(".kolecko").attributes.alt.value; //tohle by teoreticky nemusela být funkc, ale jen konstanta; takhle je to alečitelnější
+  return document.querySelector(".kolecko").attributes.alt.value;
 };
 
 const zobrazIkonu = (ikona, pole) => {
-  pole.classList.add(`tlacitko--${ikona}`); //add je funkce; kolecko je string, musí bejt v uvozovkách, undefined je, že to nic nevrací
+  pole.classList.add(`tlacitko--${ikona}`);
 };
 
 const zmenHrace = () => {
@@ -36,59 +36,3 @@ const zmenHrace = () => {
     document.querySelector(".kolecko").attributes.alt.value = "kolecko";
   }
 };
-//   if (kdoJeNaTahu(".kolecko")) {
-//   } else {
-//   }
-// };
-
-//mohly bychom mít parametry, ale bez něj to bude lepší
-
-//vyměnili jsme ("kolecko" za (`tlacitko--${ikona}`), aby se to mohlo podle potřeby měnit)
-
-/* document.querySelector(".ctverce").addEventListener("click", (event) => {
-    console.log(event.target.classList);
-    console.log(jePolePrazdne(event.target));
-    //event.target = dali jsme tam pak to pole,tady ale musíme volat to event.target
-  });
-  
-  const jePolePrazdne = (pole) => {
-    if (
-      pole.classList.contains("tlacitko--krizek") ||
-      pole.classList.contains("tlacitko--kolecko")
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }; */
-
-/* "use strict";
-
-document.querySelector(".ctverce").addEventListener("click", (event) => {
-  console.log(jePolePrazdne(event.target)); //event.target = dali jsme tam pak to pole,tady ale musíme volat to event.target
-  console.log(kdoJeNaTahu());
-  console.log(zobrazIkonu("krizek", event.target));
-});
-
-const jePolePrazdne = (pole) => {
-  if (
-    pole.classList.contains("tlacitko--krizek") ||
-    pole.classList.contains("tlacitko--kolecko")
-  ) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const kdoJeNaTahu = () => {
-  return document.querySelector(".kolecko").attributes.alt.value; //tohle by teoreticky nemusela být funkc, ale jen konstanta; takhle je to alečitelnější
-};
-
-const zobrazIkonu = (ikona, pole) => {
-  pole.classList.add(`tlacitko--${ikona}`); //add je funkce; kolecko je string, musí bejt v uvozovkách, undefined je, že to nic nevrací
-}; */
-
-//addEventListener – anonymní funkce (event) – další funkce
-
-// event.target – vrátí mi to do console, kam se kliklo;  i když listener byl na celej  čtverec, target ukáže, kde se kliklo
